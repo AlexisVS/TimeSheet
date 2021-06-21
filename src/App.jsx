fbcbe10c8a3a95d6227c3db541e49582bea695c9
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-06-22 01:55:29 +0200

    stack finie

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div>
-        <i class="fas fa-address-book"></i>
-        <h1>bonjour</h1>
-      </div>
+      <>
+        <div className={["d-flex flex-row w-100"]}>
+          <div className={["bg-primary w-10"]}>
+            sdsdsdsd
+          </div>
+          <div className={["bg-success w-70"]}>
+            <i class="fas fa-address-book"></i>
+            <h1 style={{ "fontFamily": "Poppins-semibold" }}>bonjour</h1>
+          </div>
+        </div>
+      </>
     );
   }
 }
