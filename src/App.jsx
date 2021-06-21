cce8f0f45795de5df5bf19b7f553ceea841bc1eb
AlexisVS <alexis.vansan1440@gmail.com>
2021-06-22 00:29:41 +0200

    debug

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <i class="fas fa-address-book"></i>
+        <h1>bonjour</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
