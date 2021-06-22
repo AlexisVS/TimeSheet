c67489fc1d69f140b35f734664389dfd7e49e4ba
Blaxion <80962519+Blaxion@users.noreply.github.com>
2021-06-22 14:36:53 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
   render () {
     return (
-      <>
-        <div className={["d-flex flex-row w-100"]}>
-          <div className={["bg-primary w-10"]}>
-            sdsdsdsd
-          </div>
-          <div className={["bg-success w-70"]}>
-            <i class="fas fa-address-book"></i>
-            <h1 style={{ "fontFamily": "Poppins-semibold" }}>bonjour</h1>
-          </div>
-        </div>
-      </>
+      <div>
+        
+      </div>
     );
   }
 }
